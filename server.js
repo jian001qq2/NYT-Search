dd8308e20df7cf2439f97a9150ece2a0018d1575
Jianhong <stjianhweng@gamil.com>
2019-01-24 13:59:07 -0500

    initial deployment

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,15 +12,29 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
 
-// Define API routes here
+//Mongo DB conncection
+//--------database configuration with Mongoose------------
+let databaseUrl = 'mongodb://localhost/week18day3mongoose';
+if(process.env.MONGODB_URI){
+mongoose.connect(process.env.MONGODB_URI);
+}else {
+mongoose.connect(databaseUrl)
+}
+// --end database configuration -------
+var db = mongoose.connection;
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+//show any mongoose errors
+db.on('error',function(err){
+console.log('Mongoose Error: ' , err);
+})
+//once logged in to the db through mongoose, log a success message
+db.once('open', function(){
+console.log('Mongoose connection successful.')
+})
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
